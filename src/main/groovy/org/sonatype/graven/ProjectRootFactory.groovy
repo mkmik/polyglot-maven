d416c9fa32e3c3b7563e8b7c978afb6678d6188c
Jason Dillon <jason@planet57.com>
2009-10-10 17:17:44 +0700

    Remove asserts here, executes code which isn't really needed unlike java assert

--- src/main/groovy/org/sonatype/graven/ProjectRootFactory.groovy
+++ src/main/groovy/org/sonatype/graven/ProjectRootFactory.groovy
@@ -10,8 +10,8 @@ public class ProjectRootFactory
     // NOTE: I have no clue ATM how to implement this in Java, so we use some Groovy glue here to create the proper closure
     
     static Closure create(final Script script, final BuilderSupport builder) {
-        assert script != null
-        assert builder != null
+        // assert script != null
+        // assert builder != null
 
         return {Closure c ->
             builder.project {
