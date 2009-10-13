d170121a421a7a1a08aea2299c0855e9961a9395
Jason Dillon <jason@planet57.com>
2009-10-13 20:39:35 +0700

    Add our translated pom.groovy

---
+++ pom.groovy
@@ -0,0 +1,162 @@
+project  {
+    modelVersion '4.0.0'
+    groupId 'org.sonatype.graven'
+    artifactId 'graven'
+    version '1.0-SNAPSHOT'
+    name 'Graven'
+    build  {
+        defaultGoal 'install'
+        plugins  {
+            plugin  {
+                artifactId 'maven-surefire-plugin'
+                version '2.4.3'
+                configuration  {
+                    redirectTestOutputToFile 'true'
+                    forkMode 'once'
+                    argLine '-ea'
+                    failIfNoTests 'false'
+                    workingDirectory '${project.build.directory}'
+                    excludes  {
+                        exclude '**/Abstract*.java'
+                        exclude '**/Test*.java'
+                    }
+                    includes  {
+                        include '**/*Test.java'
+                    }
+                }
+            }
+            plugin  {
+                artifactId 'maven-compiler-plugin'
+                version '2.0.2'
+                configuration  {
+                    source '1.5'
+                    target '1.5'
+                }
+            }
+            plugin  {
+                groupId 'org.codehaus.groovy.maven'
+                artifactId 'gmaven-plugin'
+                version '1.0'
+                executions  {
+                    execution  {
+                        goals  {
+                            goal 'generateStubs'
+                            goal 'compile'
+                            goal 'generateTestStubs'
+                            goal 'testCompile'
+                        }
+                    }
+                }
+                configuration  {
+                    providerSelection '1.6'
+                }
+            }
+            plugin  {
+                groupId 'org.codehaus.plexus'
+                artifactId 'plexus-component-metadata'
+                version '1.4.0-SNAPSHOT'
+                executions  {
+                    execution  {
+                        goals  {
+                            goal 'generate-metadata'
+                            goal 'generate-test-metadata'
+                        }
+                    }
+                }
+            }
+            plugin  {
+                artifactId 'maven-dependency-plugin'
+                executions  {
+                    execution  {
+                        phase 'process-sources'
+                        goals  {
+                            goal 'unpack'
+                        }
+                        configuration  {
+                            outputAbsoluteArtifactFilename 'false'
+                            artifactItems  {
+                                artifactItem  {
+                                    groupId 'org.apache.maven'
+                                    artifactId 'apache-maven'
+                                    classifier 'bin'
+                                    type 'zip'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            plugin  {
+                artifactId 'maven-assembly-plugin'
+                executions  {
+                    execution  {
+                        phase 'package'
+                        goals  {
+                            goal 'single'
+                        }
+                        configuration  {
+                            descriptors  {
+                                descriptor 'src/main/assembly/bin.xml'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    dependencies  {
+        dependency  {
+            groupId 'org.apache.maven'
+            artifactId 'maven-model-builder'
+            version '${mavenVersion}'
+        }
+        dependency  {
+            groupId 'org.apache.maven'
+            artifactId 'maven-embedder'
+            version '${mavenVersion}'
+        }
+        dependency  {
+            groupId 'org.codehaus.groovy'
+            artifactId 'groovy'
+            version '1.6.5'
+            exclusions  {
+                exclusion  {
+                    artifactId 'jline'
+                    groupId 'jline'
+                }
+                exclusion  {
+                    artifactId 'junit'
+                    groupId 'junit'
+                }
+                exclusion  {
+                    artifactId 'ant'
+                    groupId 'org.apache.ant'
+                }
+                exclusion  {
+                    artifactId 'ant-launcher'
+                    groupId 'org.apache.ant'
+                }
+            }
+        }
+        dependency  {
+            groupId 'junit'
+            artifactId 'junit'
+            version '4.7'
+            scope 'test'
+        }
+    }
+    dependencyManagement  {
+        dependencies  {
+            dependency  {
+                groupId 'org.apache.maven'
+                artifactId 'apache-maven'
+                version '${mavenVersion}'
+                type 'zip'
+                classifier 'bin'
+            }
+        }
+    }
+    properties  {
+        mavenVersion '3.0-SNAPSHOT'
+    }
+}
