f18eb0e1c79877b85ef46210bb044c6a509656a2
Mark Derricutt <mark@talios.com>
2009-12-20 22:54:33 +1300

    Removed old clojuremodel.clj file

--- pmaven-clojure/src/main/resources/org/sonatype/maven/polyglot/clojure/clojuremodel.clj
+++
@@ -1,6 +0,0 @@
-;; Macros to build up a maven Model
-
-(use 'org.sonatype.maven.polyglot.clojure.dsl)
-;(use 'org.sonatype.maven.polyglot.clojure.dsl.reference)
-;(use 'org.sonatype.maven.polyglot.clojure.dsl.dependency)
-;(use 'org.sonatype.maven.polyglot.clojure.dsl.plugin)
