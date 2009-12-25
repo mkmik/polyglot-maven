a2e16f740ebe1c37fba6fb49361bb15977f9d6c0
U-LEONARDO\Brian <Brian@Leonardo.(none)>
2009-12-25 06:55:37 -0800

    Plexus mapping for Scala

--- pmaven-scala/src/main/scala/org/sonatype/maven/polyglot/scala/ScalaModelReader.scala
+++ pmaven-scala/src/main/scala/org/sonatype/maven/polyglot/scala/ScalaModelReader.scala
@@ -1,13 +1,18 @@
-package org.sonatype.maven.polyglot
-package scala
+package org.sonatype.maven.polyglot.scala
 
-import io.ModelReaderSupport
 import java.io.Reader
 import java.util.{Map => JavaMap}
-import org.apache.maven.model.Model
+import org.apache.maven.model.io.ModelReader
+import org.sonatype.maven.polyglot.io.ModelReaderSupport
+import org.sonatype.maven.polyglot.scala.model._
+import org.codehaus.plexus.component.annotations.{Component, Requirement}
+import org.codehaus.plexus.logging.Logger
 
+@Component(role=classOf[ModelReader], hint="scala")
 class ScalaModelReader extends ModelReaderSupport {
 
+  @Requirement val logger: Logger = null
+  
   override def read(input: Reader, options: JavaMap[String, _]) = {
     require(input != null, "Illegal argument: input must not be null!")
 
