46369901a08d0e2b6877e841acace59faee76408
Jason Dillon <jason@planet57.com>
2010-01-27 15:46:15 +0700

    Need to externalize this version...

--- pmaven-common/src/main/java/org/sonatype/maven/polyglot/execute/ExecuteManagerImpl.java
+++ pmaven-common/src/main/java/org/sonatype/maven/polyglot/execute/ExecuteManagerImpl.java
@@ -92,7 +92,7 @@ public class ExecuteManagerImpl
         Plugin plugin = new Plugin();
         plugin.setGroupId("org.sonatype.pmaven");
         plugin.setArtifactId("pmaven-maven-plugin");
-        plugin.setVersion("0.7-SNAPSHOT");
+        plugin.setVersion("0.8-SNAPSHOT");
         model.getBuild().addPlugin(plugin);
 
         List<String> goals = Collections.singletonList("execute");
